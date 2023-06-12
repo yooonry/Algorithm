75db4a5e5aa2c03b09046da31ae928e89f7a7527
rydbs <117563202+rydbs@users.noreply.github.com>
2023-06-12 16:56:57 +0900

    Update Lv1.평균구하기.py

--- 2023/Programmers/Python/Lv1.평균구하기.py
+++ 2023/Programmers/Python/Lv1.평균구하기.py
@@ -1,5 +1,5 @@
 # 01 평균구하기 
-# [평균구하기]https://school.programmers.co.kr/learn/courses/30/lessons/12944?language=python3
+# https://school.programmers.co.kr/learn/courses/30/lessons/12944?language=python3
 
 def solution(arr):
     answer = sum(arr) / len(arr)
