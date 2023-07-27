ee82f0a495b7d97ac19b93a4c5285c62446f3a61
rydbs <117563202+rydbs@users.noreply.github.com>
2023-07-27 16:13:10 +0900

    Create Lv1.2016년.py

---
+++ 2023/Programmers/Python/Level1/Lv1.2016년.py
@@ -0,0 +1,12 @@
+# Level1 Programmers
+# 04 2016년
+
+def solution(a, b):
+    count=0
+    num=0
+    day=['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']
+    month=[31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    for i in range(a-1):
+        count+=month[i]
+    num=(count+b)%7
+    return day[num]
